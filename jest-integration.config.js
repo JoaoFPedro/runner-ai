0778a2504a47cd07d29c9b4f5da1ca2a8bc9ce0f
Joao Pedro <joao.fpsilva98@gmail.com>
2026-06-26 22:30:07 -0300

    chore(jest)

---
+++ jest-integration.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  preset: '@shelf/jest-mongodb',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.integration.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/main/**',
+    '!src/**/*.spec.ts',
+    '!src/**/*.test.ts',
+    '!src/**/index.ts',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react',
+        esModuleInterop: true,
+      },
+    }],
+  },
+  verbose: true,
+  testTimeout: 30000,
+};
---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/main/**',
+    '!src/**/*.spec.ts',
+    '!src/**/*.test.ts',
+    '!src/**/index.ts',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react',
+        esModuleInterop: true,
+      },
+    }],
+  },
+  verbose: true,
+  testTimeout: 10000,
+};
